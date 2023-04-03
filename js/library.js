31ce89ac85ab0af74d1278daf7157f2aa83ccc94
Chris Keilberg <83353741+ckeilberg57@users.noreply.github.com>
2023-04-03 15:14:49 -0400

    Update library.js

--- js/library.js
+++ js/library.js
@@ -1963,7 +1963,7 @@ var reg = {
             document.getElementById("conference").value = incoming_data["conference_alias"];
             document.getElementById("worker").value = this.node;
             document.getElementById("pin").value = "";
-            initialise(this.node, incoming_data["conference_alias"], undefined, "Rouding Room 114", undefined, incoming_data["token"]);
+            initialise(this.node, incoming_data["conference_alias"], undefined, "Rounding Room 114", undefined, incoming_data["token"]);
 
           //  var confirm_string=window.confirm;
           //  window.confirm=function(){
